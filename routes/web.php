a73c8b507cb4899060626c262b72f5dba6f419d7
destroylord <masapin68@gmail.com>
2021-02-06 10:53:59 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+// use Illuminate\Support\Facades\Route;
+
+
+$router->get('foo', function(){
+    return 'hello world';
+});
+
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
